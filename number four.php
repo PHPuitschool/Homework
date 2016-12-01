2cd8613a3742e34c4b6abbecc5350cba6e82204c
Dmitrij Klimenko <danglerdm@yandex.ru>
2016-12-01 22:21:24 +0200

    create project

---
+++ number four.php
@@ -0,0 +1,29 @@
+<?php
+    $age = 15 ;
+
+    switch ($age)
+    {
+    case ($age > 100):
+        echo "It is time you die" ;
+        break ;
+    case ($age >= 60):
+        echo "Time for you to retire" ;
+        break ;
+    case ($age >= 21):
+        echo "You have to go to work" ;
+        break ;
+    case ($age >= 15):
+        echo "It is time you learn at the institute" ;
+        break ;
+    case ($age >=6):
+        echo "It is time you learn at school" ;
+        break ;
+    case ($age > 1.5):
+        echo "It is time you to kindergarten" ;
+        break ;
+    case ($age < 1.5):
+        echo "Suck tits and scream" ;
+        break ;
+    }
+
+

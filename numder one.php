34892c1bf340002626f76092f3866eb8fdcc2114
Dmitrij Klimenko <danglerdm@yandex.ru>
2016-12-01 20:05:48 +0200

    create project

---
+++ numder one.php
@@ -0,0 +1,10 @@
+<?php
+    $name = "Дмитрий" ;
+    $surname = "Клименко" ;
+    $age = 35 ;
+
+    echo "Меня зовут $name <br>" ;
+    echo "Моя фамилия $surname <br>" ;
+    echo  "Мне $age лет <br>" ;
+
+

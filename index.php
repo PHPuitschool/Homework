e550f5ff8acc91dd0149a19029b7d711f8f3e533
Dmitrij Klimenko <danglerdm@yandex.ru>
2016-12-12 18:44:10 +0200

    Working version without bootstrap

--- index.php
+++ index.php
@@ -1,32 +1,33 @@
 <?php
 $errors = [];
 if (isset($_POST['first'])) {
-    $first = $_POST ['first'];
-    if (gettype($first) != 'integer') ;
+    $first = ($_POST ['first']);
+    if (!preg_match ('/[0-9]/',$first))
     {
-        $errors [] = "ERROR please enter first number<br>";
+        $errors [] = "ERROR please enter first number correctly<br>";
     }
 } else {
     $first = '';
 }
-if (isset($_POST['second'])) {
-    $second = $_POST ['second'];
-    if (gettype($second) != 'integer') ;
-    {
-        $errors [] = "ERROR please enter second number<br>";
-    }
-} else {
-    $second = '';
-}
 if (isset($_POST['calculation'])) {
     $calculation = $_POST ['calculation'];
-    if ($calculation != ("+" | "-" | "*" | "/")) ;
+    if (!preg_match ('/(\+|\-|\/|\*)+/',$calculation))
     {
         $errors [] = "ERROR please enter the sign of action<br>";
     }
 } else {
     $calculation = '';
 }
+if (isset($_POST['second'])) {
+    $second = $_POST ['second'];
+    if (!preg_match ('/[0-9]/',$second))
+    {
+        $errors [] = "ERROR please enter second number correctly <br>";
+    }
+} else {
+    $second = '';
+}
+
 if (!empty($errors)) {
     foreach ($errors as $error) {
         echo "<div style='color: red;'>$error</div>";
@@ -35,9 +36,9 @@ if (!empty($errors)) {
 
 $form = <<<FORM
 <form action="index.php" method="post">
- <p>First number: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="first" value="$first" />
- <p>Calculation of mark : <input type="text" name="calculation" value="$calculation" /></p>
- <p>Second number:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <input type="text" name="second" value="$second" /></p>
+ <p>First number:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="first" value="$first" />
+ <p>Sign of action:&nbsp;&nbsp;&nbsp; <input type="text" name="calculation" value="$calculation" /></p>
+ <p>Second number:&nbsp;<input type="text" name="second" value="$second" /></p>
  <p><input type="submit" /></p>
 </form>
 FORM;
@@ -45,19 +46,16 @@ echo $form;
 echo "<a href='index.php'>Back</a><br>";
 switch ($calculation) {
     case '+':
-        echo "The result is " . ($first + $second);
+        echo "The result is: ", ($first + $second);
         break;
     case '-':
-        echo "The result is " . ($first - $second);
+        echo "The result is: ", ($first - $second);
         break;
     case '*':
-        echo "The result is " . $first * $second;
+        echo "The result is: ", $first * $second;
         break;
     case '/':
-        echo "The result is " . $first / $second;
+        echo "The result is: ",  $first / $second;
         break;
-    //  default:
-    //      echo "ERROR please enter the sign of action<br>";
 }
-
-?>.
+?>
